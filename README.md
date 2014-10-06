351bbc6641e374b82968308b6344fa3aeedb8212
Ramunas Urbonas <r.urbonas@adform.com>
2014-10-06 10:43:27 +0300

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ adform-masterdata-dotnet-example
 ================================
 
 This is an example of .net C# application, which explains how to consume MasterData file service. You need a working adform account and MasterData service enabled for you.
+
+To automate MasterData download you will be required to perform these steps:
+1.	Authenticate with Adform security and receive authorization ticket, which will be used for further interactions 
+2.	Retrieve file list for your clients
+3.	Download each file individually
+
+More technical details on the steps:
+1.	To Authenticate with Adform security you will need to: 
+a.	Send HTTP POST request to http://inlb.app.adform.com:50054/v1/auth/login url with following application/json content:
+{"Username":"user","Password":"password"}
+b.	If you‘re credentials are valid, you will receive a JSON response:
+{"Ticket":"ticket"}
+c.	You will need this ticket for further interactions
+
+2.	To retrieve MasterData file list you will need to:
+a.	Send HTTP GET request to http://masterdata.adform.com:8652/list/xxx?render=json&authTicket=zzz 
+b.	Where xxx is your MasterData id and zzz is authetication ticket your received in step #1
+c.	If you have provided valid authorization ticket and you have right to access MasterData, you will receive a JSON response: 
+{ "meta": { "path":"/download/xxx/meta.zip", "absolutePath":"http://masterdata.adform.com:8652/xxx/download/meta.zip", "size":"123", "created":"October 06, 2014 05:00:09" },
+  "files": [ 
+    { "path":"/download/xxx/TrackingPoints/file1.zip", "absolutePath":"http://masterdata.adform.com:8652/download/xxx/TrackingPoints/file1.zip", "size":"123", "created":"October 06, 2014 05:00:09" },
+    { "path":"/download/xxx/TrackingPoints/file2.zip", "absolutePath":"http://masterdata.adform.com:8652/download/xxx/TrackingPoints/file2.zip", "size":"123", "created":"October 06, 2014 05:00:09" } 
+  ] }
+d.	MasterData meta file is a separate entry. It contains multiple files with meta information for your account. It is optional though and might be missing if service has just been enabled.
+e.	Each file is represented by path – relative url to the file location, asolutePath – absolute url to file location, size – file size in bytes, create – date, when file was created.
+
+3.	To download MasterData files you will need to:
+a.	Send HTTP GET request to absolutePath of each file, that was listed in step #2. Please note, that you will need to attach autorization ticket to each absolutePath you received. 
+b.	For example, whe you have a absolutePath: http://masterdata.adform.com:8652/download/xxx/TrackingPoints/file1.zip you will need to send request to http://masterdata.adform.com:8652/download/xxx/TrackingPoints/file1.zip?authTicket=zzz
+c.	Open response stream and save contents to local file
---
+++ src/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace DownloadAdformMasterData
+{
+    internal class Program
+    {
+        private const string AdformSecurityUrl = @"http://inlb.app.adform.com:50054/v1/auth/login";
+        private const string AdformMasterDataUrl = @"http://masterdata.adform.com:8652";
+
+        private static int Main(string[] args)
+        {
+            if (args.Length == 4)
+            {
+                var listId = Int32.Parse(args[0]);
+                var user = args[1];
+                var password = args[2];
+                var targetDirectory = args[3];
+
+                Console.WriteLine("Authenticating with Adform security...");
+                var ticket = GetTicket(user, password);
+                if (String.IsNullOrEmpty(ticket))
+                    throw new InvalidOperationException("Failed to retrieve authentication ticket");
+                
+                Console.WriteLine("Listing MasterData files...");
+                var list = GetFileList(listId, ticket);
+
+                Console.WriteLine("Downloading missing files...");
+                DownloadMissingFiles(list, ticket, targetDirectory);
+
+                Console.WriteLine("Finished.");
+            }
+            else
+            {
+                Console.WriteLine("Usage of the program:");
+                Console.WriteLine("[executable] [client-division-id] [adform-user] [adform-password] [target-directory]");
+                Console.WriteLine("For example:");
+                Console.WriteLine("[executable] 123 user password c:/temp/md-123");
+                return -1;
+            }
+
+            return 0;
+        }
+
+        private static String GetTicket(String user, String password)
+        {
+            var wr = WebRequest.Create(AdformSecurityUrl);
+
+            wr.Method = "POST";
+            wr.ContentType = "application/json";
+
+            var loginRequest = new
+            {
+                Username = user,
+                Password = password,
+            };
+
+            using (var stream = wr.GetRequestStream())
+                WriteObject(stream, loginRequest);
+            
+            var response = (HttpWebResponse) wr.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new InvalidOperationException("Unexpected response status: " + response.StatusCode);
+
+            using (var stream = response.GetResponseStream())
+                return ReadObject<LoginResponseDTO>(stream, (int) response.ContentLength).Ticket;
+        }
+
+        private static FileListDTO GetFileList(int listId, string ticket)
+        {
+            var wr = WebRequest.Create(AdformMasterDataUrl + "/list/" + listId + "?render=json&authTicket=" + ticket);
+
+            var response = (HttpWebResponse)wr.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new InvalidOperationException("Unexpected response status: " + response.StatusCode);
+
+            using (var stream = response.GetResponseStream())
+                return ReadObject<FileListDTO>(stream, (int)response.ContentLength);
+        }
+
+        private static void DownloadMissingFiles(FileListDTO list, string ticket, string targetDirectory)
+        {
+            // lets make sure target directory exists
+            var target = new DirectoryInfo(targetDirectory);
+            if (!target.Exists)
+                target.Create();
+
+            if (list.meta != null)
+                DownloadFile(list.meta, ticket, target);
+
+            if (list.files != null)
+            {
+                foreach (var file in list.files)
+                    DownloadFile(file, ticket, target);
+            }
+        }
+
+        private static void DownloadFile(FileDTO file, string ticket, DirectoryInfo targetDirectory)
+        {
+            var fileName = Path.GetFileName(file.absolutePath);
+            if (fileName == null)
+                throw new NullReferenceException("Invalid file name in path: " + file.absolutePath);
+
+            // skip if we have already have it
+            if (targetDirectory.GetFiles(fileName).Length != 0)
+                return;
+
+            var wr = WebRequest.Create(file.absolutePath + "?authTicket=" + ticket);
+            var response = (HttpWebResponse)wr.GetResponse();
+            
+            using (var downloadStream = response.GetResponseStream())
+            {
+                if (downloadStream == null)
+                    throw new NullReferenceException("Could not retrieve response stream");
+
+                var targetFile = new FileInfo(targetDirectory.FullName + "\\" + fileName);
+                try
+                {
+                    using (var fileStream = targetFile.Create())
+                    {
+                        int read;
+                        var buffer = new byte[1024];
+                        while ((read = downloadStream.Read(buffer, 0, buffer.Length)) > 0)
+                            fileStream.Write(buffer, 0, read);
+                    }
+                }
+                catch
+                {
+                    if (targetFile.Exists)
+                        targetFile.Delete();
+                    throw;
+                }
+            }
+
+        }
+
+        private static void WriteObject(Stream stream, object obj)
+        {
+            var json = new JavaScriptSerializer().Serialize(obj);
+            var bytes = Encoding.UTF8.GetBytes(json);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        private static T ReadObject<T>(Stream stream, int length)
+        {
+            var bytes = new byte[length];
+
+            var read = 0;
+            while (read < length)
+                read += stream.Read(bytes, read, length-read);
+
+            var json = Encoding.UTF8.GetString(bytes);
+
+            var serializer = new JavaScriptSerializer();
+            return serializer.Deserialize<T>(json);
+        }
+        
+        class LoginResponseDTO
+        {
+            public string Ticket;
+        }
+
+        class FileListDTO
+        {
+            public FileDTO meta;
+            public FileDTO[] files;
+        }
+
+
+        class FileDTO
+        {
+            public string path;
+            public string absolutePath;
+            public long size;
+            public string created;
+        }
+    }
+}
